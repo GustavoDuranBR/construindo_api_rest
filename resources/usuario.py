9d576110f324fb5e7b0f1d1b8f401d0efbaf8860
Gustavo Duran <gustavoduran22@gmail.com>
2022-07-13 16:27:43 -0300

    Implementando cadastro de usuários GET e DELETE de Usuário

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help='The field "login" cannot be left blank')
+atributos.add_argument('senha', type=str, required=True, help='The field "senha" cannot be left blank')
+
 
 class User(Resource):
     # /usuarios/{user_id}
@@ -20,12 +24,10 @@ class User(Resource):
             return {'message': 'User deleted.'}
         return {'message': 'User not found.'}, 404
 
+
 class UserRegister(Resource):
     # /cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help='The field "login" cannot be left blank')
-        atributos.add_argument('senha', type=str, required=True, help='The field "senha" cannot be left blank')
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -33,5 +35,13 @@ class UserRegister(Resource):
 
         user = UserModel(**dados)
         user.save_user()
-        return {'message': 'User cread successfully!'}, 200 #Created
+        return {'message': 'User created successfully!'}, 201 #Created
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+
 
